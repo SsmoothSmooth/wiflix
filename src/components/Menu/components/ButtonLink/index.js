0e34ca9a8eab84a9efa13a2c484e0f837acbe59a
José Wilson de Lima <josewilson791@gmail.com>
2020-08-05 23:32:42 -0300

    Aula 3 finalizada

--- src/components/Menu/components/ButtonLink/index.js
+++ src/components/Menu/components/ButtonLink/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
 function ButtonLink(props) {
-    // prop => { className: "0 que alguém passar", href:"/" }
-    console.log(props);
-    return (
-        <a href={props.href} className={props.className}>
-            {props.children}
-        </a>
-    )
-
+  // props => { className: "o que alguém passar", href: "/" }
+  return (
+    <a href={props.href} className={props.className}>
+      {props.children}
+    </a>
+  );
 }
 
 export default ButtonLink;
